2fe4126a95f7e75114833877796509a40f87e28f
SKWCRD <teaytoey05@gmail.com>
2018-06-18 00:35:05 +0700

    display for select symbol before play game

--- Java/Play/Display/Select.java
+++ Java/Play/Display/Select.java
@@ -1,30 +1,22 @@
 package Play.Display;
 import Play.Interfacing.InDisplay;
-
 public class Select implements InDisplay { //list OX for show out
-
     private String[] list;
-
     public Select(){
         list = new String[]{"X", "O"};
     }
-
     public String[] getList() {
         return list;
     }
-
     public String getList(int index) {
         return list[index];
     }
-
     @Override
     public String output() {
         return "Select symbol of first player.";
     }
-
     @Override
     public String title() {
         return "PLAYER [X/O]";
     }
-
 }
