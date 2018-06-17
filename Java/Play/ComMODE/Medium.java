d895df107c34e05526cc50e2764d3cee2bd3f975
SKWCRD <teaytoey05@gmail.com>
2018-06-18 01:00:55 +0700

    medium mode

--- Java/Play/ComMODE/Medium.java
+++ Java/Play/ComMODE/Medium.java
@@ -1,20 +1,15 @@
 package Play.ComMODE;
 import Play.Player;
-
 public class Medium {
-
     public Medium(Player person) {
         int pos;
         pos = ((int) (Math.round(Math.random() * 10))) % 2;
         switch (pos) {
             case 0 : new Easy(person);
                 break;
-
             case 1 : new Hard(person);
                 break;
-
-                default: new Medium(person);
+            default : new Medium(person);
         }
     }
-
 }
