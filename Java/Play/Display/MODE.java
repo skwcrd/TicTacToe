afe5b46fb1338581f2703a31c383b652a41c730b
SKWCRD <teaytoey05@gmail.com>
2018-06-18 00:24:18 +0700

    display for selection computer mode play

--- Java/Play/Display/MODE.java
+++ Java/Play/Display/MODE.java
@@ -1,30 +1,22 @@
 package Play.Display;
 import Play.Interfacing.InDisplay;
-
 public class MODE implements InDisplay { //list mode for show out
-
     private String[] list;
-
     public MODE(){
         list = new String[]{"Easy", "Medium", "Hard", "Very Hard"};
     }
-
     public String[] getList() {
         return list;
     }
-
     public String getList(int index) {
         return list[index];
     }
-
     @Override
     public String output() {
         return "Select mode for playing.";
     }
-
     @Override
     public String title() {
         return "MODE";
     }
-
 }
