ad52bafc24f5901b52fecf40784db8082603cde0
SKWCRD <teaytoey05@gmail.com>
2018-06-18 00:58:49 +0700

    very hard mode

--- Java/Play/ComMODE/VeryHard.java
+++ Java/Play/ComMODE/VeryHard.java
@@ -2,27 +2,22 @@ package Play.ComMODE;
 import Play.Check.CheckPattern;
 import Play.Check.ChecktoWin;
 import Play.Player;
-
 public class VeryHard {
-
     public VeryHard(Player person) {
         int pos;
         ChecktoWin toWin = new ChecktoWin();
         CheckPattern pattern = new CheckPattern(person);
         pos = toWin.check(person,2);
-        if(pos>0) {
+        if(pos>0)
             person.setData(person.p2,pos);
-        }
         else {
             pos = toWin.check(person,1);
-            if(pos>0) {
+            if(pos>0)
                 person.setData(person.p2,pos);
-            }
             else {
                 pos = pattern.search();
                 person.setData(person.p2, pos);
             }
         }
     }
-
 }
